ef1d053918c3a5a54a7404838d0a93bae48f8c43
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2022-07-10 11:26:29 +0200

    More codehealth fixes. I should start working on features now

--- app/src/androidTest/java/com/walagran/wwf/ui/GameActivityTest.kt
+++ app/src/androidTest/java/com/walagran/wwf/ui/GameActivityTest.kt
@@ -7,7 +7,8 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.walagran.wwf.R
@@ -17,31 +18,74 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class GameActivityTest () {
+class GameActivityTest {
 
     private val context: Context = ApplicationProvider.getApplicationContext()
 
     @Test
     fun keyboardKeyPress() {
-        launchActivityWithGameCode()
+        launchActivityWithGameCode("FLAGS")
         pressKeys("a")
         onView(withId(R.id.game_cell_11)).check(matches(withText("A")))
     }
 
     @Test
-    fun finishGameFromGameCodeIntent() {
-        launchActivityWithGameCode()
+    fun winGame_inFirstTurn_fromGameCodeIntent() {
+        launchActivityWithGameCode("FLASH")
+
         pressKeys("f", "l", "a", "s", "h", "enter")
-        onView(withId(R.id.play_game_end_game_text)).check(matches(isDisplayed()))
+
+        checkWonGame()
+    }
+
+    @Test
+    fun winGame_inLastTurn_fromGameCodeIntent() {
+        launchActivityWithGameCode("FLAGS")
+
+        pressKeys("f", "l", "a", "g", "s", "enter")
+        pressKeys("f", "l", "a", "g", "s", "enter")
+        pressKeys("f", "l", "a", "g", "s", "enter")
+        pressKeys("f", "l", "a", "g", "s", "enter")
+        pressKeys("f", "l", "a", "g", "s", "enter")
+
+        checkWonGame()
+    }
+
+    @Test
+    fun loseGame_fromGameCodeIntent() {
+        launchActivityWithGameCode("FLASH")
+
+        pressKeys("f", "l", "a", "g", "s", "enter")
+        pressKeys("f", "l", "a", "g", "s", "enter")
+        pressKeys("f", "l", "a", "g", "s", "enter")
+        pressKeys("f", "l", "a", "g", "s", "enter")
+        pressKeys("f", "l", "a", "g", "s", "enter")
+
+        checkLostGame()
+    }
+
+    private fun checkWonGame() {
+        onView(withId(R.id.play_game_end_game_text))
+            .check(matches(withText("YOU WON!")))
+    }
+
+    private fun checkLostGame() {
+        onView(withId(R.id.play_game_end_game_text))
+            .check(matches(withText("BETTER LUCK NEXT TIME ...")))
     }
 
     private fun pressKeys(vararg keys: String) {
-        keys.forEach { onView(withId(context.resources.getIdentifier("key_$it", "id", context.packageName))).perform(
-            click()) }
+        keys.forEach {
+            onView(withId(context.resources.getIdentifier("key_$it",
+                "id",
+                context.packageName)))
+                .perform(click())
+        }
     }
 
-    private fun launchActivityWithGameCode() {
-        val intent = Intent(context, GameActivity::class.java).putExtra("GAME_CODE", "FLASH")
+    private fun launchActivityWithGameCode(gameCode: String) {
+        val intent = Intent(context, GameActivity::class.java)
+            .putExtra("GAME_CODE", gameCode)
         ActivityScenario.launch<GameActivity>(intent)
     }
 }
--- app/src/main/java/com/walagran/wwf/ui/GameActivity.kt
+++ app/src/main/java/com/walagran/wwf/ui/GameActivity.kt
@@ -67,9 +67,13 @@ class GameActivity : AppCompatActivity() {
     }
 
     /**
-     * Creates a text view grid with empty boxes and legit IDs. Also populates a grid for the actual input characters.
+     * Creates a text view grid with empty boxes and legit IDs. Also populates a
+     * grid for the actual input characters.
      */
     private fun createGrid() {
+        /**
+         * Apply style to the textView.
+         */
         fun applyCellStyle(textView: TextView) {
             val layoutParams = TableRow.LayoutParams(
                 resources!!.getDimension(R.dimen.game_cell_size).toInt(),
@@ -84,6 +88,9 @@ class GameActivity : AppCompatActivity() {
             textView.setTextColor(resources!!.getColor(R.color.black))
         }
 
+        /**
+         * Apply style to a row of textViews.
+         */
         fun applyRowStyle(tableRow: TableRow) {
             val rowLayoutParams =
                 TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT,
@@ -92,16 +99,20 @@ class GameActivity : AppCompatActivity() {
         }
 
         val layoutGrid = findViewById<TableLayout>(R.id.play_game_table)
-        for (i in 1..5) {
+        for (i in 1..MAX_WORDS) {
             val textViewGridCacheRow = ArrayList<TextView>()
             val layoutRow = TableRow(this)
             applyRowStyle(layoutRow)
-            layoutRow.id = resources.getIdentifier("game_row_$i", "id",
+            layoutRow.id = resources.getIdentifier(
+                "game_row_$i",
+                "id",
                 packageName)
-            for (j in 1..5) {
+            for (j in 1..MAX_LETTERS) {
                 val cell = TextView(this)
-                cell.id = resources.getIdentifier("game_cell_$i$j",
-                    "id", packageName)
+                cell.id = resources.getIdentifier(
+                    "game_cell_$i$j",
+                    "id",
+                    packageName)
                 applyCellStyle(cell)
                 layoutRow.addView(cell)
                 textViewGridCacheRow.add(cell)
@@ -119,7 +130,8 @@ class GameActivity : AppCompatActivity() {
     }
 
     /**
-     * Gets the game code from an intent. This could either be from a URL or from a parameter.
+     * Gets the game code from an intent. This could either be from a URL or
+     * from a parameter.
      */
     private fun getCorrectWord(savedInstanceState: Bundle?) {
         if (intent.action == Intent.ACTION_VIEW) {
@@ -130,9 +142,8 @@ class GameActivity : AppCompatActivity() {
             }
         } else {
             if (savedInstanceState == null) {
-                val extras = intent.extras
-                if (extras != null) {
-                    correctWord = extras.getString("GAME_CODE")!!
+                if (intent.extras != null) {
+                    correctWord = intent.extras!!.getString("GAME_CODE")!!
                 }
             } else {
                 correctWord =
@@ -145,30 +156,39 @@ class GameActivity : AppCompatActivity() {
 
     internal inner class PlayGameKeyboardEventListener : KeyboardEventListener {
         override fun onAlphaKeyPressed(alphabet: Char) {
-            if (gameEnded || rowInFocus == 5 || cellInFocus == 5) {
+            // Check if ended or beyond last cell
+            if (gameEnded || rowInFocus == MAX_WORDS || cellInFocus == MAX_LETTERS) {
                 return
             }
+
+            // Update state
             textViewGridCache[rowInFocus][cellInFocus].text =
-                alphabet.toString()
-            textGrid[rowInFocus][cellInFocus] =
-                Character.toUpperCase(alphabet)
+                alphabet.uppercase()
+            textGrid[rowInFocus][cellInFocus] = alphabet.uppercaseChar()
+
+            // Move cursor
             cellInFocus++
         }
 
         override fun onBackKeyPressed() {
-            if (gameEnded || rowInFocus == 5 || cellInFocus == 0) {
+            // Check if ended or before first cell
+            if (gameEnded || rowInFocus == MAX_WORDS || cellInFocus == 0) {
                 return
             }
-            cellInFocus--
+
+            // Update state
             textViewGridCache[rowInFocus][cellInFocus].text = ""
+
+            // Move cursor
+            cellInFocus--
         }
 
         override fun onEnterKeyPressed() {
-            fun highlightLetters() {
+            fun processWordAndHighlightLetters() {
                 val word: CharArray = correctWord.toCharArray()
                 Log.d("Something", correctWord)
                 val marked = booleanArrayOf(false, false, false, false, false)
-                for (i in 0..4) {
+                for (i in 0 until MAX_LETTERS) {
                     val currentLetter = textGrid[rowInFocus][i]
                     if (currentLetter == word[i]) {
                         textViewGridCache[rowInFocus][i].setBackgroundColor(
@@ -177,7 +197,7 @@ class GameActivity : AppCompatActivity() {
                         marked[i] = true
                     }
                 }
-                for (i in 0..4) {
+                for (i in 0 until MAX_LETTERS) {
                     if (marked[i]) continue
                     val currentLetter = textGrid[rowInFocus][i]
                     for (j in 0..4) {
@@ -193,8 +213,11 @@ class GameActivity : AppCompatActivity() {
             }
 
             fun endGame(win: Boolean) {
-                val endGameText = findViewById<TextView>(R.id.play_game_end_game_text)
-                endGameText.text = if (win) "YOU WON!" else "BETTER LUCK NEXT TIME ..."
+                // ToDo: Make ending the game better.
+                val endGameText =
+                    findViewById<TextView>(R.id.play_game_end_game_text)
+                endGameText.text =
+                    if (win) "YOU WON!" else "BETTER LUCK NEXT TIME ..."
                 endGameText.visibility = View.VISIBLE
                 val shareButtons =
                     findViewById<FrameLayout>(R.id.play_game_share_buttons)
@@ -203,22 +226,29 @@ class GameActivity : AppCompatActivity() {
             }
 
             fun getWordFromRowInFocus(): Optional<String> {
-                return if (cellInFocus == 5) Optional.of(TextUtils.join("",
+                return if (cellInFocus == MAX_LETTERS) Optional.of(TextUtils.join(
+                    "",
                     textGrid[rowInFocus])) else Optional.empty()
             }
 
-            if (gameEnded || rowInFocus == 5) {
+            // Check if game ended
+            if (gameEnded || rowInFocus == MAX_WORDS) {
                 return
             }
-            getWordFromRowInFocus().ifPresent { word: String ->
-                if (word == correctWord) {
+            // Get a word if the row is complete or do nothing.
+            getWordFromRowInFocus().ifPresent {
+                // The word is correct.
+                if (it == correctWord) {
                     endGame(true)
                 }
-                if (Utils.isWordValid(applicationContext, word)) {
-                    highlightLetters()
+
+                // The word is a valid word of the english dictionary
+                if (Utils.isWordValid(applicationContext, it)) {
+                    processWordAndHighlightLetters()
                     rowInFocus++
                     cellInFocus = 0
-                    if (rowInFocus == 5) {
+                    // No more rows left.
+                    if (rowInFocus == MAX_WORDS) {
                         endGame(false)
                     }
                 } else {
@@ -227,5 +257,10 @@ class GameActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    companion object {
+        const val MAX_WORDS = 5
+        const val MAX_LETTERS = 5
     }
 }
